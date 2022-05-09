95de7409b623cfc9868e92376b623b15f1e97f43
0xbwill <william.degh@gmail.com>
2022-05-10 00:04:39 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div className="about">
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dolorum
+        voluptate placeat quisquam sunt blanditiis illum sapiente fugit, ipsum
+        totam, consequatur dignissimos ad adipisci distinctio fuga alias
+        explicabo, repellendus repellat ratione minima? Dolorem quibusdam nobis
+        iusto quas, nostrum hic earum harum distinctio rem, nesciunt, pariatur
+        laudantium mollitia. Tempora, ab reprehenderit?
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo quibusdam
+        nisi, tempore doloremque dolores recusandae ex obcaecati eum ea iusto
+        accusamus molestias unde aliquid blanditiis eveniet autem deleniti quas
+        odio veritatis repudiandae harum fugit. Sapiente iusto molestiae sunt
+        doloribus in laudantium odio accusamus consectetur, corrupti tenetur
+        maiores, error provident dolor?
+      </p>
+    </div>
+  );
+};
+
+export default About;
