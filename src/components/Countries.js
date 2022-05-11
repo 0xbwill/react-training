c56a24b6fcd6e6baea8ef05a60990b057c8a4f5c
botwilly13 <william.degh@gmail.com>
2022-05-11 16:38:53 +0200

    Update Countries.js

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,8 +3,14 @@ import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
 const Countries = () => {
+  // useState permettant de stocker l'array venant de l'API
+  // 'data' est la constante, 'setData' est son setter 
+  // on doit passer par lui pour faire évoluer la valeur de data
   const [data, setData] = useState([]);
+
   const [rangeValue, setRangeValue] = useState([36]);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
 
   useEffect(() => {
     axios
@@ -22,11 +28,31 @@ const Countries = () => {
           defaultValue={rangeValue}
           onChange={(e) => setRangeValue(e.target.value)}
         />
-      </ul>
-      <ul>
-        {data.slice(0, rangeValue).map((country, index) => (
-          <Card key={index} country={country} />
+
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent == selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
